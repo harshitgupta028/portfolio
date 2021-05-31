cdd70da5a8848daaf20cd8ba309f24b1b3469241
harshit gupta <harshit.28.sps@gmail.com>
2021-06-01 02:21:54 +0530

    NavBar

---
+++ app.js
@@ -0,0 +1,14 @@
+
+window.onscroll = ()=> {scrollFunction()};
+
+function scrollFunction() {
+  // console.log(document.documentElement.scrollTop);
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("navbar").style.top = "0";
+    if (document.documentElement.scrollTop > 1784){
+      console.log("Education");
+    }
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
